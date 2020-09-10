da544574f8b58e49bb9a8d17dccb5d2c7ce888ab
Ekaterina Manich <katenkarudzenia@gmail.com>
2020-09-10 21:36:33 +0300

    feat/2: fix posts link and add logout icon

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -30,17 +30,19 @@ function Posts(props) {
       {loading ? <div className="posts-loading">
         <div id="loading"></div>
         <div style={{
-          fontSize:"20px",
-          marginTop:"5px"
+          fontSize: "20px",
+          marginTop: "5px"
         }}>Please wait...</div>
       </div> : <>
-        {posts.map((item) => (
-          <div className="post" key={item.id} onClick={()=>props.history.push(`/posts/${item.id}`)}>
-            <div className="post-title">{item.id}. {item.title}</div>
-            <div>{item.body}</div>
-            <button onClick={() => deletePost(item.id)}>Delete post</button>
-          </div>))}
-      </>}
+          {posts.map((item) => (
+            <div className="post" key={item.id}>
+              <div onClick={() => props.history.push(`/posts/${item.id}`)}>
+                <div className="post-title">{item.id}. {item.title}</div>
+                <div>{item.body}</div>
+              </div>
+              <button onClick={() => deletePost(item.id)}>Delete post</button>
+            </div>))}
+        </>}
       {error && <div>Request to server error</div>}
     </div>
   )
